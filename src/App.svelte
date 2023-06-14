ce591c0b42a38423941958b5daf0e6f16dc17e77
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-06-14 17:17:54 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { onMount, createEventDispatcher } from "svelte";
+  import { onMount } from "svelte";
   import "bootstrap/dist/css/bootstrap.min.css";
   import DevExpress from "devextreme";
 
   let jsonData = [];
   let gridData = [];
- 
+
   onMount(async () => {
     const response = await fetch(
       "https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
@@ -21,17 +21,66 @@
       mobile: item.mobile,
     }));
 
-   
-       const columns= [
-          { dataField: "id", caption: "ID", width: 250 },
-          { dataField: "firstName", caption: "First Name", width: 180 },
-          { dataField: "surname", caption: "Surname", width: 180 },
-          { dataField: "email", caption: "Email", width: 180 },
-          { dataField: "mobile", caption: "Mobile", width: 100 },
-          {
-            caption: "Actions",
-            width: 350,
-            cellTemplate: function (container, options) {
+    const columns = [
+      { dataField: "id", caption: "ID", width: 250 },
+      { dataField: "firstName", caption: "Full Name", width: 200 },
+      { dataField: "surname", caption: "Surname", width: 200 },
+      { dataField: "email", caption: "Email", width: 200 },
+      { dataField: "mobile", caption: "Mobile", width: 150 },
+      {
+        caption: "Actions",
+        width: 400,
+        cellTemplate: function (container, options) {
+          const downloadButton = document.createElement("button");
+          downloadButton.innerText = "Download CV";
+          downloadButton.addEventListener("click", async () => {
+            const cvResponse = await fetch(
+              `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
+            );
+            if (cvResponse.ok) {
+              const cvData = await cvResponse.json();
+              const cvId = cvData.internal.cloudFile.id;
+              const downloadLink = `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${cvId}&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77`;
+              window.open(downloadLink);
+            } else {
+              alert("Failed to fetch CV file.");
+            }
+          });
+          container.appendChild(downloadButton);
+
+          const viewButton = document.createElement("button");
+          viewButton.innerText = "View CV";
+          viewButton.addEventListener("click", async () => {
+            const cvResponse = await fetch(
+              `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
+            );
+            if (cvResponse.ok) {
+              const cvData = await cvResponse.json();
+              const cvHtml = cvData.html;
+              if (cvHtml) {
+                const cvWindow = window.open("", "_blank");
+                cvWindow.document.write(cvHtml);
+                cvWindow.document.close();
+                closeButton.addEventListener("click", () => {
+                  document.body.removeChild(popupContainer);
+                });
+                popupContainer.appendChild(closeButton);
+
+                const cvContent = document.createElement("div");
+                cvContent.innerHTML = cvHtml;
+                popupContainer.appendChild(cvContent);
+
+                document.body.appendChild(popupContainer);
+              } else {
+                alert("CV file not found.");
+              }
+            } else {
+              alert("Failed to fetch CV file.");
+            }
+          });
+          container.appendChild(viewButton);
+       
+
           const uploadButton = document.createElement("button");
           uploadButton.innerText = "Upload CV";
           uploadButton.addEventListener("click", async () => {
@@ -61,56 +110,11 @@
             fileInput.click();
           });
           container.appendChild(uploadButton);
-              
-              
-              const downloadButton = document.createElement("button");
-          downloadButton.innerText = "Download CV";
-          downloadButton.addEventListener("click", async () => {
-            const cvResponse = await fetch(
-              `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
-            );
-            if (cvResponse.ok) {
-              const cvData = await cvResponse.json();
-              console.log(cvData);
-              const cvId = cvData.internal.cloudFile.id;
-              console.log(cvData.internal.cloudFile);
-              console.log(cvId);
-
-              const downloadLink = `https://api.recruitly.io/api/cloudfile/download?cloudFileId=${cvId}&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77`;
-              window.open(downloadLink);
-            } else {
-              alert("Failed to fetch CV file.");
-            }
-          });
-          container.appendChild(downloadButton);
-              
-          const viewButton = document.createElement("button");
-          viewButton.innerText = "View CV";
-          viewButton.addEventListener("click", async () => {
-            const cvResponse = await fetch(
-              `https://api.recruitly.io/api/candidatecv/${options.data.id}?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA`
-            );
-            if (cvResponse.ok) {
-              const cvData = await cvResponse.json();
-              const cvHtml = cvData.html;
-              if (cvHtml) {
-                const cvWindow = window.open("", "_blank");
-                cvWindow.document.write(cvHtml);
-                cvWindow.document.close();
-              } else {
-                alert("CV file not found.");
-              }
-            } else {
-              alert("Failed to fetch .");
-            }
-          });
-          container.appendChild(viewButton);
         },
-        width: 250,
+        width: 400,
       },
       // Add other columns as needed
     ];
-
 
     const dataGrid = new DevExpress.ui.dxDataGrid(
       document.getElementById("dataGrid"),
@@ -140,12 +144,11 @@
             confirmDeleteMessage:
               "Are you sure you want to delete this record?",
           },
-          onSaveRowChanges: handleSave, // Bind handleSave function to the saveRowChanges event
         },
         paging: {
           pageSize: 10,
         },
-       onRowInserting: async (e) => {
+        onRowInserting: async (e) => {
 			console.log("Data being sent to API:", e.data);
 			try {
 			  const response = await fetch(
@@ -229,49 +232,33 @@
           console.error("Failed to delete record:", error);
         }
       },
-        onInitialized: () => {
-          // Function called when the grid is initialized
-          // ...
-        },
+        onInitialized: () => {},
       }
     );
   });
 </script>
 
-<div id="dataGrid"></div>
-
-
-
 <style>
-  .popup-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .popup-content {
-    background-color: white;
-    padding: 20px;
-    border-radius: 4px;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-    width: 400px;
-  }
-
-  .popup-content h3 {
-    margin-top: 0;
-  }
-
-  .btn {
-    margin-top: 10px;
-  }
-
   #dataGrid {
-    margin-top: 20px;
+    height: 400px;
+  }
+
+  .popup-container {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 80%;
+    height: 80%;
+    background-color: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    padding: 20px;
+    overflow: auto;
+  }
+
+  .popup-container button {
+    display: block;
+    margin-bottom: 10px;
   }
 </style>
+<div id="dataGrid"></div>
